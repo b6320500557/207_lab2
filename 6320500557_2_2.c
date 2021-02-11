34fbb2a5fedc881576497aef3af866ea6726cb4f
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-02-11 15:34:14 +0700

    คิดค่าโทร

--- 6320500557_2_2.c
+++ 6320500557_2_2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int pro;
+    int min;
+    float total;
+    scanf("%c",&pro);
+    scanf("%d",&min);
 
+    if(pro == 'A')
+    {
+        total = min % 200;
+        printf("%.2f",total);
+    }
+
+}
